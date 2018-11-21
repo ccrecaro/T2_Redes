2a5a269bad0d6c69c0db6969e6b1425e3d480c78
ccrecaro <ccre.caro@gmail.com>
2018-11-20 21:40:35 -0300

    Cambio receiver

--- myreceiver.py
+++ myreceiver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
 from socket import *
 import pickle
 import hashlib
@@ -6,75 +8,59 @@ import os
 import math
 import time
 
+import random
 #takes the port number as command line arguments and create server socket
 serverIP=""
 serverPort=int(sys.argv[2])
 
 serverSocket=socket(AF_INET,SOCK_DGRAM)
 serverSocket.bind((serverIP,serverPort))
-serverSocket.settimeout(3)
+serverSocket.settimeout(2)
 print "Ready to serve"
 
 #initializes packet variables 
 expectedseqnum=1
-ACK=1
-ack = []
-
+windowSize=7;
+ack = 0
 #RECEIVES DATA
 f = open("output", "wb")
 endoffile = False
 lastpktreceived = time.time()	
 starttime = time.time()
 
+#############################################################################
+#
+# ESTABLECER CONEXION
+#
+#############################################################################
+
+retries = 0
+
 while True:
-
 	try:
-		rcvpkt=[]
 		packet,clientAddress= serverSocket.recvfrom(4096)
-		rcvpkt = pickle.loads(packet)
-#		check value of checksum received (c) against checksum calculated (h) - NOT CORRUPT
-		c = rcvpkt[-1]
-		del rcvpkt[-1]
-		h = hashlib.md5()
-		h.update(pickle.dumps(rcvpkt))
-		if c == h.digest():
-#		check value of expected seq number against seq number received - IN ORDER 
-			if(rcvpkt[0]==expectedseqnum):
-				print "Received inorder", expectedseqnum
-				if rcvpkt[1]:
-					f.write(rcvpkt[1])
-				else:
-					endoffile = True
-				expectedseqnum = expectedseqnum + 1
-#				create ACK (seqnum,checksum)
-				sndpkt = []
-				sndpkt.append(expectedseqnum)
-				h = hashlib.md5()
-				h.update(pickle.dumps(sndpkt))
-				sndpkt.append(h.digest())
-				serverSocket.sendto(pickle.dumps(sndpkt),clientAddress)
-				print "New Ack", expectedseqnum
-
-			else:
-#		default? discard packet and resend ACK for most recently received inorder pkt
-				print "Received out of order", rcvpkt[0]
-				sndpkt = []
-				sndpkt.append(expectedseqnum)
-				h = hashlib.md5()
-				h.update(pickle.dumps(sndpkt))
-				sndpkt.append(h.digest())
-				serverSocket.sendto(pickle.dumps(sndpkt), clientAddress)
-				print "Ack", expectedseqnum
-		else:
-			print "error detected"
 	except:
-		if endoffile:
-			if(time.time()-lastpktreceived>3):
-				break
-
-
-endtime = time.time()
-
+		retries+=1
+		if retries>10:
+			print "conexion terminada por exceso de re-intentos"
+			break
+		continue
+	
+	#############################################################################
+	#
+	# TERMINAR LA CONEXION SI SE RECIBE INDICADOR DE TERMINO DE CONEXION
+	#
+	#############################################################################
+	
+	
+	(file_name, total_size, nextSeqnum, isData, data) = packet.split("|||")
+	
+	
+	if str(nextSeqnum) == str(ack):
+		serverSocket.sendto(str(ack),clientAddress)
+		ack = (ack + 1) % (windowSize+1)
+		f.write(data)
+		retries=0
+	
+		
 f.close()
-print 'FILE TRANFER SUCCESSFUL'
-print "TIME TAKEN " , str(endtime - starttime)
